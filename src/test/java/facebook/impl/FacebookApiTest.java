df234a231b04f540060059016b8587d85b23ca98
Piotr Kamiński <piotrkaminski111@gmail.com>
2017-06-26 22:29:59 +0200

    Simple tests implementation

--- src/test/java/facebook/impl/FacebookApiTest.java
+++ src/test/java/facebook/impl/FacebookApiTest.java
@@ -4,11 +4,24 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import facebook.profile.Facebook;
+
 public class FacebookApiTest {
 
+	@Test(expected=NotFoundException.class)
+	public void whenNotExistingIdGivenNotFoundExceptionThrown() throws NotFoundException {
+		FacebookApi facebookApi = new FacebookApi();	
+		facebookApi.findById("12333");
+		
+	}
+	
 	@Test
-	public void test() {
-		assertTrue(true);
+	public void shouldReturnInstanceOfFacebookObject() throws NotFoundException {
+		FacebookApi facebookApi = new FacebookApi();	
+		Facebook profile = facebookApi.findById("1");
+
+		assertTrue(profile instanceof Facebook);
+		
 	}
 
 }
